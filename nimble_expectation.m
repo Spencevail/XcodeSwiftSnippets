849e4d1b32f3623e688416e464f4c2f645481630
Spencer Comerford <Spencer@Spen.co>
2015-04-17 13:51:15 -0600

    Imported user code snippets

---
+++ nimble_expectation.m
@@ -0,0 +1,10 @@
+// Nimble Expectation
+// 
+//
+// IDECodeSnippetCompletionPrefix: nimspec
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 577195FD-2F91-40DB-B847-3F7575969CAC
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+expect(<#actual#>).to<#Not#>(<#equal#>(<#expected#>))
